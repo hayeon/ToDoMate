31e712ac1a88e038afc2e93459acc9f5f20f932a
hayeon <hayun4475@gmail.com>
2022-08-05 17:34:10 +0900

    todo textinput

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { theme } from "./theme";
 
-export default function App() {
+ function App() {
+  const [headerToggle, setHeaderToggle] = useState("work");
+  const work = () => setHeaderToggle("work");
+  const travel = () => setHeaderToggle("travel");
+  const [toDo, setTodo] = useState("");
+  const onSubmit = () => {
+    alert(toDo);
+  }
+
+  const onChange = (e) => console.log(e); 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        {/* 터치시 발생하는 이벤트 */}
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{
+              ...styles.headerText,
+              color: headerToggle === "work" ? "rgb(255,255,255)" : theme.grey,
+            }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.headerText,
+              color:
+                headerToggle === "travel" ? "rgb(255,255,255)" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput 
+        onSubmitEditing={onSubmit}
+        onChangeText={onChange}
+        returnKeyType="done"
+        value={toDo}
+        multiline
+        placeholder={
+          headerToggle === "work"
+            ? "오늘의 할 일을 정리하세요"
+            : "어디로 떠나고 싶으세요?"
+        }
+        style={{ ...styles.input }}
+      />
     </View>
   );
 }
@@ -13,8 +67,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.backgroundColor,
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: "row",
+    marginTop: 100,
+    justifyContent: "space-between",
+  },
+  headerText: {
+    fontSize: 30,
+    color: "white",
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    borderRadius: 10,
+    marginTop: 30,
+    fontSize: 20,
   },
 });
+
+export default App;
---
+++ theme.js
@@ -0,0 +1,4 @@
+export const theme = {
+    backgroundColor:"black",
+    grey: "#3A3D48",
+};
